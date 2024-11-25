ae63c8d910eee7460579755ebb12ec71df7fa58b
J. Scott Cardinal <archaeo_scott@yahoo.com>
2024-11-24 19:45:08 -0500

    Update Justeson_repro.R

--- Justeson_repro.R
+++ Justeson_repro.R
@@ -1,24 +1,56 @@
-M<-c(100,82,74,69,62,55,53,51,45,42,38,38,36,34,34,34,33,33,32,32,31,30,29,28,25,23,22,22,22,22,21,20,20,20,20,20,18,14,14,12,12,12,10,10,10,10,10,8,6,6,1,1,1,1,1) # from Justeson (1973)
+M <- c(
+  100,82,74,69,62,55,53,51,45,42,38,38,36,34,34,34,33,33,
+  32,32,31,30,29,28,25,23,22,22,22,22,21,20,20,20,20,20,
+  18,14,14,12,12,12,10,10,10,10,10,8,6,6,1,1,1,1,1
+  ) # from Justeson (1973)
 
-M<-c(M,rep(1,808)) # make L[1] = 2,317
+M <- c(M, rep(1, 808)) # make L[1] = 2,317
 
 codes <- c(1:100)
 
-code_fq <-rep(1,100)
+# Extract frequency of codes from M
+code_fq <- rep(1, 100)
 
-for(i in 2:length(M)) {
-  code_fq <- c(code_fq[1:(length(code_fq) - M[i])], code_fq[((length(code_fq) - M[i]) + 1):100] + 1)
+for (i in 2:length(M)) {
+  code_fq <- c(
+    code_fq[1:(length(code_fq) - M[i])], 
+    code_fq[((length(code_fq) - M[i]) + 1):100] + 1
+    )
 }
 
+# Extract N from code frequencies
+
+N <- c()
+
+for(i in 1:length(M)) {
+  N[i] <- length(code_fq[code_fq == i])
+}
+
+# Get t
+
+t <- c(1:max(code_fq))
+
+# Calculate L
+
 L <- c()
+
 for (t in 1:length(M)) {
   L[t] <- sum(M[t:length(M)])
 }
 
-plot(1:length(M),M, type="o", log="y", xlim=c(0,70))
-plot(1:length(L),L, type="o", log="y", xlim=c(0,70))
+plot(1:length(M),
+     M,
+     type = "o",
+     log = "y",
+     xlim = c(0, 70))
 
-(L[1]^2 / (L[1]-M[1]))-L[1]
+plot(1:length(L),
+     L,
+     type = "o",
+     log = "y",
+     xlim = c(0, 70))
 
-r<- rank(-code_fq, ties.method = "random")
+(L[1] ^ 2 / (L[1] - M[1])) - L[1]
+
+r <- rank(-code_fq, ties.method = "average")
 
