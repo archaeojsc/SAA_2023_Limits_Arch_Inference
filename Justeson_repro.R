95d83c01d1a251fd7cbc32a2e5ba5c9215bc6c04
J. Scott Cardinal <archaeo_scott@yahoo.com>
2024-12-03 23:38:12 -0500

    Update Justeson_repro.R

--- Justeson_repro.R
+++ Justeson_repro.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(ggplot2)
+library(caret)
+
 M <- c(
   100,82,74,69,62,55,53,51,45,42,38,38,36,34,34,34,33,33,
   32,32,31,30,29,28,25,23,22,22,22,22,21,20,20,20,20,20,
@@ -12,15 +16,32 @@ codes <- c(1:(max(M)))
 code_fq <- rep(1, length(codes))
 
 for (i in 2:length(M)) {
-  code_fq <- c(
-    code_fq[1:(length(code_fq) - M[i])], 
-    code_fq[((length(code_fq) - M[i]) + 1):length(code_fq)] + 1
-    )
+  code_fq <- c(code_fq[1:(length(code_fq) - M[i])], code_fq[((length(code_fq) - M[i]) + 1):length(code_fq)] + 1)
 }
+
+# Zipf models
+library(zipfR)
+
+joint_site.tfl < zipfR::tfl(f = code_fq) # term frequency list
+joint_site.spc <- tfl2spc(joint_site.tfl) # frequency spectra
+
+#  Large-Number-of-Rare-Event (LNRE) model
+joint_site.fzm <- lnre(
+  type = "fzm",
+  spc = joint_site.spc,
+  cost = "exact",
+  bootstrap = 100,
+  method = "SANN",
+  parallel = 4
+)
+
+joint_site.fzm.spc <- lnre.spc(joint_site.fzm)
+
+
 
 # Feature scaling p(r)
 alpha <- 0.001
-code_fq_scaled <- (code_fq - min(code_fq)) / (max(code_fq) - min(code_fq)) + alpha
+code_fq_norm <- (code_fq - min(code_fq)) / (max(code_fq) - min(code_fq)) #+ alpha
 
 # Extract N from code frequencies
 
@@ -42,21 +63,25 @@ for (i in 1:length(M)) {
   L[i] <- sum(M[i:length(M)])
 }
 
-plot(t[1:55],
-     M[1:55],
-     type = "o",
-     log = "y",
-     xlim = c(0, 55),
-     ylim = c(1, 200),
-     xlab = "t",
-     ylab = "M")
+plot(
+  t[1:55],
+  M[1:55],
+  type = "o",
+  log = "y",
+  xlim = c(0, 55),
+  ylim = c(1, 200),
+  xlab = "t",
+  ylab = "M"
+)
 
-plot(t,
-     L,
-     type = "o",
-     log = "y",
-     xlim = c(0, 70),
-     ylim = c(500,3000))
+plot(
+  t,
+  L,
+  type = "o",
+  log = "y",
+  xlim = c(0, 70),
+  ylim = c(500, 3000)
+)
 
 (L[1] ^ 2 / (L[1] - M[1])) - L[1]
 
@@ -65,14 +90,16 @@ plot(t,
 
 # r <- rank(-code_fq_scaled, ties.method = "max")
 # r <- rank(-code_fq_scaled, ties.method = "average")
-r <- rank(-code_fq_scaled, ties.method = "random")
+r <- rank(-code_fq, ties.method = "random")
 # r <- rank(-code_fq_scaled, ties.method = "min")
 
-rank_freq <- data.frame(rank = r[1:99], frequency = code_fq_scaled[1:99])
+rank_freq <- data.frame(code = codes[1:99],
+                        frequency = code_fq[1:99],
+                        rank = r[1:99])
 
-rlm2 <- lm(code_fq_scaled[1:99] ~ poly(r[1:99], 2, raw = TRUE))
-rlm3 <- lm(code_fq_scaled[1:99] ~ poly(r[1:99], 3, raw = TRUE))
-rlm4 <- lm(code_fq_scaled[1:99] ~ poly(r[1:99], 4, raw = TRUE))
+rlm2 <- lm(frequency ~ poly(rank, 2, raw = TRUE), data = rank_freq)
+rlm3 <- lm(frequency ~ poly(rank, 3, raw = TRUE), data = rank_freq)
+rlm4 <- lm(frequency ~ poly(rank, 4, raw = TRUE), data = rank_freq)
 
 
 plot(c(0, code_fq_scaled[1:99]),
